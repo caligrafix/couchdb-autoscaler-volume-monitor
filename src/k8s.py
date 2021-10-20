a0ed6b3a61f49735f414c8bba1a9b17ddeae7215
Francisco Alberto <lemufen@gmail.com>
2021-10-20 08:56:17 -0300

    Update scenario 4: Monitor all volumes in statefulset

--- src/k8s.py
+++ src/k8s.py
@@ -88,7 +88,21 @@ def patch_namespaced_pvc(namespace, pod_pvc_info, spec_body):
         logging.info(f"api response: {api_response}")
 
 
-def execute_exec_pod(exec_command, namespace, pod):
+def execute_exec_pods(exec_command, namespace, pod):
     resp = stream(v1.connect_get_namespaced_pod_exec, pod, namespace,
                   command=exec_command, stderr=True, stdin=False, stdout=True, tty=False)
     return resp
+
+
+def get_pods_volumes_info(namespace, pods, mount_volume_path):
+    exec_command = ['df', '-Ph', mount_volume_path]
+    pods_volumes_info = {}
+
+    for pod in pods:
+        # Execute command df inside each POD to obtain %Use of volume associated
+        resp = execute_exec_pods(exec_command, namespace, pod)
+        df_output_lines = [s.split() for s in resp.splitlines()]
+        perc_usage = float(df_output_lines[1][4].strip('%'))/100
+        pods_volumes_info[pod] = perc_usage
+
+    return pods_volumes_info
