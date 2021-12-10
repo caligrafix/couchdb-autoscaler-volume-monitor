60e48a0dffc012311d870e72f08a9691c061be0d
Francisco Alberto <lemufen@gmail.com>
2021-12-10 11:05:22 -0300

    Add curl request to finish cluster setup in main.sh script

--- init-scripts/scripts/main.sh
+++ init-scripts/scripts/main.sh
@@ -4,6 +4,15 @@ echo 'sleeping 60 seconds'
 sleep 60
 ./tag-zone-nodes.sh
 
-echo 'sleeping 30 seconds'
+echo 'sleeping 10 seconds'
+sleep 10
+
+echo 'finish cluster setup'
+curl -s http://QIvMWnriiEmNMrih:mWclZkABuWRvTmGu@couchdb-svc-couchdb.couchdb.svc.cluster.local:5984/_cluster_setup \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"action": "finish_cluster"}'
+
+echo 'sleeping 10 seconds'
 sleep 30
 ./test-placement.sh
