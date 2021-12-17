d7065ddb50cd3f39cb2488710b1e1c6c106d4553
Francisco Alberto <lemufen@gmail.com>
2021-12-17 09:01:10 -0300

    Rename rebuild to rebuild-source-code.sh

--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete pod cluster-init
-docker build -t gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests:init-cluster . --no-cache
-docker push gitlab-registry.caligrafix.cl:443/ecaligrafix/couchdb-k8s-stress-tests:init-cluster
-kubectl create -f k8s-files/manifest/init-script.yaml
-watch kubectl get pods
