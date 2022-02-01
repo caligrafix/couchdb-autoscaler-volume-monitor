5a08e11fadae5a1a92219633d826403d48bd7308
Francisco Alberto <lemufen@gmail.com>
2022-02-01 09:00:34 -0300

    Update envs names

--- src/envs.py
+++ src/envs.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load env vars
-namespace = os.getenv('EKS_NAMESPACE')
+namespace = os.getenv('NAMESPACE')
 VOLUME_THRESHOLD = float(os.getenv('VOLUME_THRESHOLD'))
 VOLUME_RESIZE_PERCENTAGE = float(os.getenv('VOLUME_RESIZE_PERCENTAGE'))
-MOUNT_VOLUME_PATH = os.getenv('VOLUME_MOUNT_PATH')
+VOLUME_MOUNT_PATH = os.getenv('VOLUME_MOUNT_PATH')
